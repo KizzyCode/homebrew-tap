fab68f9a514b8e286f13594351adb853d6eefd18
KizzyCode <development@kizzycode.de>
2018-01-14 15:19:41 +0100

    Added working v0.2.0-alpha-release

--- kc-httpfs.rb
+++ kc-httpfs.rb
@@ -1,6 +1,10 @@
 class KcHttpfs < Formula
   desc "This application allows you to mount a HTTP-resource like a file"
   homepage "https://github.com/KizzyCode/HTTPMount"
+
+  url "https://github.com/KizzyCode/HTTPMount/archive/0.2.0.tar.gz"
+  sha256 "3bb2397fe7088124a0a1dc791603a5bc9d0503669dbf2f9c99a876da67136ed1"
+
   head "https://github.com/KizzyCode/HTTPMount.git"
 
   depends_on "rust" => :build
