2580a0fb8b55971687f5212f2825a3906c2fd3a8
Keziah Biermann <development@kizzycode.de>
2018-02-19 12:05:40 +0100

    Updated to v0.3.0 and added bottle

--- kc-cryptopipe.rb
+++ kc-cryptopipe.rb
@@ -1,15 +1,15 @@
 class KcCryptopipe < Formula
-  desc "Chunk-based authenticated en-/decryption for pipes using Argon2i as PBKDF and ChaCha20/Poly1305 as AE-cipher"
+  desc "Chunk-based authenticated en-/decryption for pipes using Argon2i as PBKDF and ChaCha20+Poly1305 as AE-cipher"
   homepage "https://github.com/KizzyCode/CryptoPipe"
 
-  url "https://github.com/KizzyCode/CryptoPipe/archive/0.2.0.tar.gz"
-  sha256 "b1578d34b63bea2f6a6f000b41d5099006b6b8beb3b670bca1de9eecc33455f2"
+  url "https://github.com/KizzyCode/CryptoPipe/archive/0.3.0.tar.gz"
+  sha256 "80aa5dfa2d09b19ba78198d1bc4af595bd0992d9e4084ba1cdfa8f77c14bd0be"
 
   head "https://github.com/KizzyCode/CryptoPipe.git"
 
   bottle do
-    root_url "https://github.com/KizzyCode/CryptoPipe/releases/download/0.2.0/"
-    sha256 "58a39a6f7089028e719179a975af60679bdd895a8f29718f08a85a9ce8138a33" => :high_sierra
+    root_url "https://github.com/KizzyCode/CryptoPipe/releases/download/0.3.0/"
+    sha256 "8d2393429f3c8f25104dc6e068e7930cd294ce3eb0a287ca8a3bfcecebaeba6c" => :high_sierra
   end
 
   depends_on "rust" => :build
@@ -17,12 +17,11 @@ class KcCryptopipe < Formula
 
   def install
     # Run self-test and build executable
-    system("cargo test --release")
-    system("cargo build --release")
+    system "cargo test --release"
+    system "cargo build --release"
 
-    # Rename and install executable
-    cp("target/release/crypto_pipe", "target/release/cryptopipe")
-    bin.install("target/release/cryptopipe")
+    # Install executable
+    bin.install "target/release/crypto_pipe" => "cryptopipe"
   end
 
   test do
