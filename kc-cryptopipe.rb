a13809c81143377372df52fa7b15dd1c16f54ada
Keziah Biermann <development@kizzycode.de>
2018-02-19 12:25:12 +0100

    Updated to 0.3.1 (emergency fix)

--- kc-cryptopipe.rb
+++ kc-cryptopipe.rb
@@ -2,15 +2,15 @@ class KcCryptopipe < Formula
   desc "Chunk-based authenticated en-/decryption for pipes using Argon2i as PBKDF and ChaCha20+Poly1305 as AE-cipher"
   homepage "https://github.com/KizzyCode/CryptoPipe"
 
-  url "https://github.com/KizzyCode/CryptoPipe/archive/0.3.0.tar.gz"
-  sha256 "80aa5dfa2d09b19ba78198d1bc4af595bd0992d9e4084ba1cdfa8f77c14bd0be"
+  url "https://github.com/KizzyCode/CryptoPipe/archive/0.3.1.tar.gz"
+  sha256 "c769b40ca4a445c5fe700e084d0acb754584eab998d0f8fbaf28132028f57868"
 
   head "https://github.com/KizzyCode/CryptoPipe.git"
 
-  bottle do
-    root_url "https://github.com/KizzyCode/CryptoPipe/releases/download/0.3.0/"
-    sha256 "8d2393429f3c8f25104dc6e068e7930cd294ce3eb0a287ca8a3bfcecebaeba6c" => :high_sierra
-  end
+  #bottle do
+  #  root_url "https://github.com/KizzyCode/CryptoPipe/releases/download/0.3.1/"
+  #  sha256 "8d2393429f3c8f25104dc6e068e7930cd294ce3eb0a287ca8a3bfcecebaeba6c" => :high_sierra
+  #end
 
   depends_on "rust" => :build
   depends_on "libsodium" => :build
