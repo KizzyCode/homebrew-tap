3bdf780ee00ada588d494ae865c3e592da361923
Keziah Biermann <development@kizzycode.de>
2018-02-19 12:30:18 +0100

    Added bottle

--- kc-cryptopipe.rb
+++ kc-cryptopipe.rb
@@ -7,10 +7,10 @@ class KcCryptopipe < Formula
 
   head "https://github.com/KizzyCode/CryptoPipe.git"
 
-  #bottle do
-  #  root_url "https://github.com/KizzyCode/CryptoPipe/releases/download/0.3.1/"
-  #  sha256 "8d2393429f3c8f25104dc6e068e7930cd294ce3eb0a287ca8a3bfcecebaeba6c" => :high_sierra
-  #end
+  bottle do
+    root_url "https://github.com/KizzyCode/CryptoPipe/releases/download/0.3.1/"
+    sha256 "441f06d5788d139add180239b5aeeb2c7039e55085346609f9a751d1047ce0e1" => :high_sierra
+  end
 
   depends_on "rust" => :build
   depends_on "libsodium" => :build
