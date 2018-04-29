faa945122e18215cba29b7fd2b8dc03060a58ba9
Keziah Biermann <development@kizzycode.de>
2018-04-29 17:12:34 +0200

    Added formula for converthex

---
+++ kc-converthex.rb
@@ -0,0 +1,13 @@
+class KcConverthex < Formula
+  desc "Converts an hex-string to a hex-string-literal/base64-string/etc."
+  homepage "https://github.com/KizzyCode/converthex"
+  head "https://github.com/KizzyCode/converthex.git"
+
+  def install
+    bin.install "convert_hex" => "converthex"
+  end
+
+  test do
+    assert system("which converthex") != true
+  end
+end
