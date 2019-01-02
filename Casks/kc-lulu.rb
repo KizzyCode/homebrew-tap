5e0b0c4f9ef26d03ce922d41bed7155f933fdd1c
Keziah Biermann <development@kizzycode.de>
2019-01-02 23:58:36 +0100

    Added kc-lulu.rb for testing purposes

---
+++ Casks/kc-lulu.rb
@@ -0,0 +1,50 @@
+cask 'lulu' do
+  version '1.1.1'
+  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+
+  # bitbucket.org/objective-see was verified as official when first introduced to the cask
+  url "https://bitbucket.org/objective-see/deploy/downloads/LuLu_#{version}.zip"
+  appcast 'https://objective-see.com/products/changelogs/LuLu.txt'
+  name 'LuLu'
+  homepage 'https://objective-see.com/products/lulu.html'
+
+  depends_on macos: '>= :sierra'
+
+  installer script: {
+                      executable: "#{staged_path}/Lulu Installer.app/Contents/MacOS/LuLu Installer",
+                      args:       ['-install'],
+                      sudo:       true,
+                    }
+
+  # Lulu's uninstall deletes its settings, so `brew cask upgrade` also does it
+  # `postflight` and `uninstall_preflight` are trying to mitigate this by backing them up and restoring
+  settings_dir = '/Library/Objective-See/LuLu'
+  backup_settings_dir = '/tmp/Homebrew_tmp_LuLu_settings'
+
+  postflight do
+    FileUtils.mv(backup_setting_dir, settings_dir) if Dir.exist?(backup_settings_dir)
+  end
+
+  uninstall_preflight do
+    FileUtils.cp_r(settings_dir, backup_settings_dir)
+  end
+
+  uninstall script: {
+                      executable: "#{staged_path}/Lulu Installer.app/Contents/MacOS/LuLu Installer",
+                      args:       ['-uninstall'],
+                      sudo:       true,
+                    }
+
+  zap trash: [
+               '~/Library/Caches/com.objective-see.lulu',
+               '~/Library/Caches/com.objective-see.luluHelper',
+               '~/Library/Preferences/com.objective-see.lulu.plist',
+               '~/Library/Preferences/com.objective-see.luluHelper.plist',
+               '/Library/LaunchDaemons/com.objective-see.lulu.plist',
+               '/Library/Logs/LuLu.log',
+             ]
+
+  caveats do
+    kext
+  end
+end
