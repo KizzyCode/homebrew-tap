b9e51d18c556f10caabd16012f8af3fc8ca6e9f7
Keziah Biermann <development@kizzycode.de>
2019-01-03 01:05:23 +0100

    Put build-dependencies into `head`-dependency section

--- kc-zbar.rb
+++ kc-zbar.rb
@@ -6,21 +6,23 @@ class KcZbar < Formula
   sha256 "6db85ae844b797d318a7f877366d6259d039691dc9f756331cd6c2a8cea511c0"
   revision 1
 
-  head "https://github.com/KizzyCode/homebrew-source-zbar.git"
-
   bottle do
     root_url "https://github.com/KizzyCode/homebrew-source-zbar/releases/download/0.20.1/"
     sha256 "84b0862f09f9829c0d70ebe0e68e0cc88971ef6eecccb84939bca2e5f34872b3" => :mojave
     sha256 "0df8290a2bb94ba1a8fbefdc1f9408b8761a269f1ea62a66462631c4d218b743" => :high_sierra
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "docbook" => :build
-  depends_on "gettext" => :build
-  depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
-  depends_on "xmlto" => :build
+  head do
+    url "https://github.com/KizzyCode/homebrew-source-zbar.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "docbook" => :build
+    depends_on "gettext" => :build
+    depends_on "libtool" => :build
+    depends_on "pkg-config" => :build
+    depends_on "xmlto" => :build
+  end
   
   depends_on "freetype"
   depends_on "imagemagick"
