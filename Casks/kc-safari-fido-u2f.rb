339c8549487b3342e02f5b0f17bd830b420e2034
Keziah Biermann <development@kizzycode.de>
2019-01-22 17:37:57 +0100

    Added formula

---
+++ Casks/kc-safari-fido-u2f.rb
@@ -0,0 +1,14 @@
+cask "kc-safari-fido-u2f" do
+  version "0.2.5"
+  sha256 "74b80df7997b6d0811a32a31fa6cc29058d0ee4027bd0f784f1d8abf9561f612"
+
+  url "https://github.com/Safari-FIDO-U2F/Safari-FIDO-U2F/releases/download/2.0b5/Safari.FIDO.U2F.zip"
+  name "Safari FIDO U2F"
+  homepage "https://github.com/Safari-FIDO-U2F/Safari-FIDO-U2F"
+
+  app "Safari FIDO U2F.app"
+
+  postflight do
+    ohai "To register the plugin, open the app, restart Safari and enable \"Safari FIDO U2F Extension\" in the extensions settings"
+  end
+end
