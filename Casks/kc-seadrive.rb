a82be72a00777eaf5949bac74887d865b075e50a
KizzyCode <development@kizzycode.de>
2019-03-20 18:43:43 +0100

    Added cask

---
+++ Casks/kc-seadrive.rb
@@ -0,0 +1,10 @@
+cask "kc-seadrive" do
+  version "1.0.3"
+  sha256 "37f31127e15bea91b2f814bbca223da4c892f38e6e0418853baadb9cfdf5c667"
+
+  url "https://download.seadrive.org/seadrive-1.0.3.dmg"
+  name "SeaDrive"
+  homepage "https://www.seafile.com/"
+
+  app "SeaDrive.app"
+end
