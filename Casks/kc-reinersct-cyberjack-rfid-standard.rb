e244caa9206e460fd8b97c84c44572400eb581ad
KizzyCode <development@kizzycode.de>
2019-05-23 16:23:06 +0200

    Fixed SHA-mismatch

--- Casks/kc-reinersct-cyberjack-rfid-standard.rb
+++ Casks/kc-reinersct-cyberjack-rfid-standard.rb
@@ -1,6 +1,6 @@
-cask 'kc-reinersct-cyberjack-rfid-standard' do
-  version '3.99.5'
-  sha256 '18fb1eab5003cf51ec6a4d8612a6a3d0e7e02f8165d06eb88cd637be527e5a16'
+cask "kc-reinersct-cyberjack-rfid-standard" do
+  version "3.99.5"
+  sha256 "25bcd5d5ff197163bba94e72cddb4b74ad8a04df8e88df01404378f80c46f9b4"
   
   url "https://support.reiner-sct.de/downloads/MAC/pcsc-cyberjack_3.99.5final.SP13-x86_64-signed.pkg"
   name "REINER SCT CyberJack RFID Standard Driver"
