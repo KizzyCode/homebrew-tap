da1b1e538fe2479f5d24acd9cee73fc906a13250
KizzyCode <development@kizzycode.de>
2020-04-15 21:33:48 +0200

    Added formula

---
+++ Casks/kc-knockknock.rb
@@ -0,0 +1,10 @@
+cask "kc-knockknock" do
+  version "2.1.1"
+  sha256 "c3293e6f6524b557246a9c5e06a1fb2eb3f49be2297fe671d0fa8fca7aa142fa"
+
+  url "https://bitbucket.org/objective-see/deploy/downloads/KnockKnock_2.1.1.zip"
+  name "Knock Knock"
+  homepage "https://objective-see.com/products/knockknock.html"
+
+  app "KnockKnock.app"
+end
