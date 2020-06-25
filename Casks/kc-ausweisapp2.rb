d471d9502fab85680f1067051f0bbdf8cde4a064
KizzyCode <development@kizzycode.de>
2020-06-25 17:19:16 +0200

    Updated version

--- Casks/kc-ausweisapp2.rb
+++ Casks/kc-ausweisapp2.rb
@@ -1,8 +1,8 @@
 cask "kc-ausweisapp2" do
-  version "1.20.0"
+  version "1.20.1"
   sha256 "76c60930e58cdf05df05c4eb0c485b1d0931a9a6dc647b4737ca1316bcf8e873"
 
-  url "https://appl.governikus-asp.de/ausweisapp2/AusweisApp2-1.20.0.dmg"
+  url "https://appl.governikus-asp.de/ausweisapp2/AusweisApp2-1.20.1.dmg"
   name "AusweisApp 2"
   homepage "https://www.ausweisapp.bund.de/ausweisapp2/"
 
