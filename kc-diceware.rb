3d624ab28301ee35d1859190785c1698a0e58f11
Keziah Biermann <development@kizzycode.de>
2020-10-06 18:09:44 +0200

    Added release and fixed build error

--- kc-diceware.rb
+++ kc-diceware.rb
@@ -2,7 +2,12 @@ class KcDiceware < Formula
   desc "An implementation of the popular diceware method to generate passwords"
   homepage "https://github.com/KizzyCode/diceware-swift"
   head "https://github.com/KizzyCode/diceware-swift.git"
-  
+
+  url "https://github.com/KizzyCode/diceware-swift/archive/1.0.0.tar.gz"
+  sha256 "ca4e7de25535c7d3b736d214a92d5c02ceb0de67259c8f4c8f4a4cd63a2cd2c8"
+
+  depends_on :xcode => "11.0"
+
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/diceware"
