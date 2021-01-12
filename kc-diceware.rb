f5c2f2dcacec49c7da3add8d7f440203927eacff
KizzyCode <development@kizzycode.de>
2021-01-13 00:38:50 +0100

    Updated version to 1.1.0

--- kc-diceware.rb
+++ kc-diceware.rb
@@ -3,10 +3,10 @@ class KcDiceware < Formula
   homepage "https://github.com/KizzyCode/diceware-swift"
   head "https://github.com/KizzyCode/diceware-swift.git"
 
-  url "https://github.com/KizzyCode/diceware-swift/archive/1.0.1.tar.gz"
-  sha256 "5acce2152220e6423fd254023905d3bfaf6bd539e22fe4feee1fc8db91dc6b57"
+  url "https://github.com/KizzyCode/diceware-swift/archive/1.1.0.tar.gz"
+  sha256 "9e250b3461ae910d0069da968c46e5a9374b61409e8300f4fdf1001e2f63031b"
 
-  depends_on :xcode => "11.0"
+  depends_on :xcode => "12.0"
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
