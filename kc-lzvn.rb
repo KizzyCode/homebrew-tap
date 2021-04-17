3406a5d36ec021e589ef0570e79f5cb20700bb02
Keziah Biermann <development@kizzycode.de>
2021-04-17 21:18:58 +0200

    Fixed HEAD and build

--- kc-lzvn.rb
+++ kc-lzvn.rb
@@ -1,10 +1,12 @@
 class KcLzvn < Formula
   desc "An LVZN compression implemention"
   homepage "https://github.com/RJVB/LZVN"
-  head "https://github.com/RJVB/LZVN"
+  head "https://github.com/RJVB/LZVN.git", :branch => "RJVB"
 
+  depends_on "cmake" => :build
 
   def install
+    system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "."
     system "make"
     system "make", "install"
   end
