60e0375eda5f99f15645afe37720475f62f846b8
Keziah Biermann <development@kizzycode.de>
2021-06-14 17:20:25 +0200

    Fixed deprecated syntax

--- kc-zbar.rb
+++ kc-zbar.rb
@@ -1,15 +1,15 @@
 class KcZbar < Formula
   desc "Suite of barcodes-reading tools (Fedora's more modern fork)"
   homepage "https://zbar.sourceforge.io"
-  
+
   url "https://github.com/KizzyCode/homebrew-source-zbar/archive/0.20.1.tar.gz"
   sha256 "6db85ae844b797d318a7f877366d6259d039691dc9f756331cd6c2a8cea511c0"
   revision 1
 
   bottle do
     root_url "https://github.com/KizzyCode/homebrew-source-zbar/releases/download/0.20.1/"
-    sha256 "84b0862f09f9829c0d70ebe0e68e0cc88971ef6eecccb84939bca2e5f34872b3" => :mojave
-    sha256 "0df8290a2bb94ba1a8fbefdc1f9408b8761a269f1ea62a66462631c4d218b743" => :high_sierra
+    sha256 mojave:      "84b0862f09f9829c0d70ebe0e68e0cc88971ef6eecccb84939bca2e5f34872b3"
+    sha256 high_sierra: "0df8290a2bb94ba1a8fbefdc1f9408b8761a269f1ea62a66462631c4d218b743"
   end
 
   head do
@@ -23,14 +23,14 @@ class KcZbar < Formula
     depends_on "pkg-config" => :build
     depends_on "xmlto" => :build
   end
-  
+
   depends_on "freetype"
   depends_on "imagemagick"
   depends_on "jpeg"
   depends_on "libtool"
   depends_on "ufraw"
   depends_on "xz"
-  
+
   def install
     gettext = Formula["gettext"]
     system "autoreconf", "-fvi", "-I", "#{gettext.opt_share}/aclocal"
