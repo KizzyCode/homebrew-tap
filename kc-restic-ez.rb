4ec02d05deb1e2a2f8dcefe29fca56cbd69527c0
Keziah Biermann <development@kizzycode.de>
2021-09-05 23:32:32 +0200

    Moved formula target to ResticEz-rust

--- kc-restic-ez.rb
+++ kc-restic-ez.rb
@@ -1,10 +1,16 @@
 class KcResticEz < Formula
-  desc "A simple restic wrapper with easy repository configuration"
-  homepage "https://github.com/KizzyCode/ResticEz-python"
-  head "https://github.com/KizzyCode/ResticEz-python.git"
+  desc "`restic-ez` provides a simple configuration loader and API around `restic`"
+  homepage "https://github.com/KizzyCode/ResticEz-rust"
+  head "https://github.com/KizzyCode/ResticEz-rust.git"
+
+  url "https://github.com/KizzyCode/ResticEz-rust/archive/refs/tags/0.1.0.tar.gz"
+  sha256 "e0a0ba18223f6608cb93d985cf89384e22c0e2b0c3f01747a8f5c7cea1020db7"
+
+  depends_on "rust" => :build
 
   def install
-    bin.install "restic-ez.py" => "restic-ez"
+    system "cargo build --release"
+    bin.install "target/release/restic-ez" => "restic-ez"
   end
 
   test do
