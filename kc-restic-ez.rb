c7625caa90fc2ade30d6ba723db00c7a084e76da
Keziah Biermann <development@kizzycode.de>
2021-09-09 19:58:27 +0200

    Bumped release

--- kc-restic-ez.rb
+++ kc-restic-ez.rb
@@ -3,8 +3,8 @@ class KcResticEz < Formula
   homepage "https://github.com/KizzyCode/ResticEz-rust"
   head "https://github.com/KizzyCode/ResticEz-rust.git"
 
-  url "https://github.com/KizzyCode/ResticEz-rust/archive/refs/tags/0.1.4.tar.gz"
-  sha256 "339f9236da2e81ab4bd39c49f1f42f5f7becfa28d6e9b5400826fb2cb70d8e3e"
+  url "https://github.com/KizzyCode/ResticEz-rust/archive/refs/tags/0.1.5.tar.gz"
+  sha256 "5d7d38657520afb9c6479516f09b6a4145b52ccf9e37a1831c9e67c84d8cc458"
 
   depends_on "rust" => :build
 
