b3576268986406f92aeeec2e76e6aafd05ca6410
Keziah Biermann <development@kizzycode.de>
2022-04-01 23:06:48 +0200

    Use the Swift replacement since macOS' shipped Python is dead

--- kc-seticon.rb
+++ kc-seticon.rb
@@ -1,13 +1,19 @@
 class KcSeticon < Formula
-  desc "Tool to set custom icons for files and folders"
-  homepage "https://github.com/KizzyCode/SetFinderIcon"
-  head "https://github.com/KizzyCode/SetFinderIcon.git"
+  desc "A tiny CLI tool to assign an icon to a folder on macOS"
+  homepage "https://github.com/KizzyCode/SetFolderIcon-swift"
+  head "https://github.com/KizzyCode/SetFolderIcon-swift.git"
+
+  url "https://github.com/KizzyCode/SetFolderIcon-swift/archive/refs/tags/0.1.0.tar.gz"
+  sha256 "794ac5e57e72ac72e75214e45b9fa1ecb49538714ec4758faeae9abf8a543180"
+
+  depends_on :xcode => "12.0"
 
   def install
-    bin.install "seticon.py" => "seticon" 
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/seticon"
   end
 
   test do
-    assert system("which seticon") != true
+    system "#{bin}/seticon"
   end
 end
