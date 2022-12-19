dfa4ba987a7e0992e76195ac9637f6b5a5c07e28
Keziah Biermann <development@kizzycode.de>
2022-12-19 22:38:59 +0100

    Update formula

--- kc-spipe.rb
+++ kc-spipe.rb
@@ -1,8 +1,8 @@
 class KcSpipe < Formula
   desc "a simple program that can be used to read from/write to a serial device node"
   homepage "https://github.com/KizzyCode/SerialPipe-c"
-  url "https://github.com/KizzyCode/SerialPipe-c/archive/refs/tags/0.1.3.tar.gz"
-  sha256 "4e7a859fef44272b044b93d71362beb568cd082eebb2db84123f5ba54656204b"
+  url "https://github.com/KizzyCode/SerialPipe-c/archive/refs/tags/0.1.4.tar.gz"
+  sha256 "425575c537bc415e73a685812e92d9523dafbb5613fd63bb1d4b5e34347a8cae"
 
   def install
     system "make"
