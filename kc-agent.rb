5c32e393bf0b288fcf4eeee501923e1c0f3512d1
Keziah Biermann <development@kizzycode.de>
2026-07-17 01:44:32 +0200

    Update formula

--- kc-agent.rb
+++ kc-agent.rb
@@ -1,10 +1,10 @@
 class KcAgent < Formula
-  desc "A tiny script to start ghcr.io/kizzycode/devcontainer:minimal as agent container"
+  desc "A tiny script to start the ghcr.io/kizzycode/devcontainer containers as agent container"
   homepage "https://github.com/KizzyCode/Devcontainer-docker"
   head "https://github.com/KizzyCode/Devcontainer-docker.git"
 
-  url "https://github.com/KizzyCode/Devcontainer-docker/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "d41fd7c1d4d20caf60209d416528e776838396f3507b1e0483d999e57c3e5c68"
+  url "https://github.com/KizzyCode/Devcontainer-docker/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "26abe92c9e2d4532acddd8ec7c5b8064b6841ee2c944ed13ba48f1b7e7047fe8"
 
   depends_on "ca-certificates"
   depends_on "container"
