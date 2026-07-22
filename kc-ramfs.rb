e96e1e4e7850e7d6cc34246bd9716f855ecdff59
Keziah Elis Biermann <development@kizzycode.de>
2026-07-22 12:56:45 +0200

    Update formula

--- kc-ramfs.rb
+++ kc-ramfs.rb
@@ -2,8 +2,8 @@ class KcRamfs < Formula
     desc "A simple script to create a in-memory filesystem on macOS"
     homepage "https://github.com/KizzyCode/ramfs-shell"
     head "https://github.com/KizzyCode/ramfs-shell.git"
-    url "https://github.com/KizzyCode/ramfs-shell/archive/refs/tags/0.1.0.tar.gz"
-    sha256 "160a6a4f1fb3ce265f7b68f364ded6eaf3a50f397f4a41a826c9d301ec1530cd"
+    url "https://github.com/KizzyCode/ramfs-shell/archive/refs/tags/0.2.0.tar.gz"
+    sha256 "0b2b5759b43a62f144ec749baac7cbd439dc35f81f49372c0147822d89aa77d7"
   
     def install
       bin.install("ramfs")
